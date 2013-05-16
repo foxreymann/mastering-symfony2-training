b2473b3d8cac675dbc051307d3dee73a4d440dfa
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-16 16:24:45 +0100

    functional tests

--- src/Sensio/Bundle/HangmanBundle/Tests/Controller/GameControllerTest.php
+++ src/Sensio/Bundle/HangmanBundle/Tests/Controller/GameControllerTest.php
@@ -26,14 +26,50 @@ class GameControllerTest extends WebTestCase
     // Write your functional test scenario here...
     public function testTryWord()
     {
-        $crawler = $this->client->request('GET','/game/');
-        $form = $crawler->selectButton('Let me guess...')->form();
-        $crawler = $this->client->submit($form, array('word' => 'php'));
+        $crawler = $this->playWord('php');
 
         $this->assertEquals(
             'Congratulations!',
             $crawler->filter('#content > h2:first-child')->text()
         );
+    }
+    
+    public function testTryInvalidWord()
+    {
+        $crawler = $this->playWord('java');
+
+        $this->assertEquals(
+            'Game Over!',
+            $crawler->filter('#content > h2:first-child')->text()
+        );
+    }
+
+    public function playWord($word) 
+    {
+        $crawler = $this->client->request('GET','/game/');
+        $form = $crawler->selectButton('Let me guess...')->form();
+        return $this->client->submit($form, array('word' => $word));
+    }
+
+    public function playLetter($letter, $crawler)
+    {
+        $link = $crawler->selectLink($letter)->link();
+        return $this->client->click($link);
+    }    
+    
+    public function testTryWinLetterByLetter()
+    {
+        $crawler = $this->client->request('GET','/game/');
+
+        $crawler = $this->playLetter('P', $crawler);
+        $crawler = $this->playLetter('X', $crawler);
+        $crawler = $this->playLetter('H', $crawler);
+        
+        $this->assertEquals(
+            'Congratulations!',
+            $crawler->filter('#content > h2:first-child')->text()
+        );
+        
     }
 
 
