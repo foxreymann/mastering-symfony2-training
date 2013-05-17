645a39bd33466819a1482259835e4b326f48e8bc
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-17 14:48:50 +0100

    starting even listeners

---
+++ src/Sensio/Bundle/HangmanBundle/Tests/EventListener/PasswordEncoderListenerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sensio\Bundle\HangmanBundle\Test\EventListener;
+
+use Sensio\Bundle\HangmanBundle\EventListener\PasswordEncoderListener;
+
+class PasswordEncoderListenerTest extends \PHPUnit_Framework_TestCase
+{
+
+
+
+}
