b4fb50125e056719306c00052a864a0752f899ab
tomaszjureczko <tomasz.jureczko@gmail.com>
2013-05-17 15:24:51 +0100

    testing event listener

---
+++ src/Sensio/Bundle/HangmanBundle/EventListener/PasswordEncoderListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sensio\Bundle\HangmanBundle\EventListener;
+
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+        use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class PasswordEncoderListener
+{
+    private $passwordEncoder;
+
+    public function __construct(PasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;    
+    }
+
+
+    public function prePersist(LifecycleEventArgs $event)
+    {
+        $event->getEntity()->encodePassword($this->passwordEncoder);       
+    }
+
+} 
--- src/Sensio/Bundle/HangmanBundle/Tests/EventListener/PasswordEncoderListenerTest.php
+++ src/Sensio/Bundle/HangmanBundle/Tests/EventListener/PasswordEncoderListenerTest.php
@@ -6,7 +6,23 @@ use Sensio\Bundle\HangmanBundle\EventListener\PasswordEncoderListener;
 
 class PasswordEncoderListenerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testItEncodesThePassword()
+    {
+        $encoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface');
 
+        $player = $this->getMock('Sensio\Bundle\HangmanBundle\Entity\Player');
+        $player->expects($this->once())
+            ->method('encodePassword')
+            ->with($encoder);
 
-
+        $event = $this->getMockBuilder('Doctrine\Common\Persistence\Event\LifecycleEventArgs')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $event->expects($this->once())
+            ->method('getEntity')
+            ->will($this->returnValue($player));
+        
+        $listener = new PasswordEncoderListener($encoder);
+        $listener->prePersist($event);
+    }
 }
